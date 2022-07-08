ee18a211cfab0198fad44127d8ecbd552d56ed16
cameronbuster <cbuster.0@gmail.com>
2022-07-08 17:09:35 -0500

    Initial Commit

---
+++ ghost-engine-cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ghost-engine-cpp
+//
+//  Created by Cameron Buster on 7/8/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
